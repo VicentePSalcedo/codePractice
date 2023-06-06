197a6869eb5a4d01bc2fd596dbd535ae8cf9ea19
vicentepsalcedo <vicentepsalcedo@gmail.com>
2023-06-06 13:16:13 -0400

    regex solution works for 2/3 test cases. I need to work on allowing zero values.

---
+++ practiceproblems/Regex.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Regex {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+    }
+}
+
+class MyRegex{
+//    So this works a little to well. If you use this it fails HackerRank's first test case because it does not permit
+//    things like "000.12.12.034".
+//    String pattern = "^(?:(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])\\.){3}"
+//            +"(?:25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9][0-9]|[0-9])$";
+    String pattern = "^((25[0-5]|(2[0-4]|1\\d|[1-9]|)\\d)\\.?\\b){4}$";
+}
